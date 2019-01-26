09f105361406a0963e897b08c13cbe5ab9b5c433
unknown <mikhail.romasev@yandex.ru>
2019-01-26 17:12:08 +0400

    It is my first commit

---
+++ project/script.js
@@ -0,0 +1,67 @@
+'use strict';
+
+console.log(Mikhail);
+
+var Mikhail = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(Mikhail);
+console.log(second);
+
+
+var Hello = "Hello, world";
+console.log(Hello);
+
+let programmer = {
+    age: 20,
+    name: "Romasev Mikhail",
+    country: "Russia"
+}
+console.log(programmer.name);
+
+let programmers = ['Romasev', 'Lymar', 'During'];
+console.log(programmers[0]);
+
+// alert("romasev.ru");
+
+// confirm("Hi");
+
+// let question = confirm("Are you a human?");
+// console.log(question);
+
+// let question = prompt("Are you a human?", "Yes");
+// console.log(typeof(question));
+
+// let question = +prompt("Are you a human?", "Yes");
+// console.log(typeof(question));
+
+console.log("arr " + "- object");
+console.log(4 + +"object");
+
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+
+console.log(incr++);
+console.log(decr--);
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+console.log("2" == 2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClosed = false;
+
+console.log(isChecked && isClosed);
+console.log(isChecked == !isClosed);
+console.log(isChecked || isClosed);
+
+
+
+
+
